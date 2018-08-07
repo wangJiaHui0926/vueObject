8af3baa74e7210bfef6b58b578908e98cb3ff6e8
wangJiaHui0926 <15097794678@163.com>
2018-08-07 11:54:19 +0800

    侧边栏点击转换页面

--- src/main.js
+++ src/main.js
@@ -2,10 +2,12 @@ import Vue from 'vue';
 import App from './App.vue';
 
 import 'bootstrap/dist/css/bootstrap.css';
-
 import './assets/css/index.css';
 
+import router from './routes/router';
+
 new Vue({
+  router,
   el: '#app',
   render: h => h(App)
-})
+});
---
+++ src/routes/router.js
@@ -0,0 +1,22 @@
+import VueRouter from 'vue-router';
+import Vue from 'vue';
+// 导入组件
+import HeroesList from '../views/heroes/List.vue';
+import EquipsList from '../views/equips/List.vue';
+import WeaponsList from '../views/weapons/List.vue';
+
+Vue.use(VueRouter);
+
+var router = new VueRouter({
+    
+    linkExactActiveClass: 'active',
+
+    routes: [
+        { path: '/', redirect: '/heroes' },
+        { name: 'heroes', path: '/heroes', component: HeroesList },
+        { name: 'equips', path: '/equips', component: EquipsList },
+        { name: 'weapons', path: '/weapons', component: WeaponsList }
+    ]
+});
+
+export default router;
